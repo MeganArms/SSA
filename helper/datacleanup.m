16ea3d6f26d3708cdc6bb4f0de420d106a5a17f4
MeganArms <megjarm1991@gmail.com>
2017-05-02 10:44:11 -0400

    Now redundant

--- helper/datacleanup.m
+++ helper/datacleanup.m
@@ -1,4 +1,4 @@
-function [lifetimes, censoring] = datacleanup(d0,dstart,obj,th,exclude)
+function [lifetimes, censoring] = datacleanup(d0,dstart,obj,th,ff,et,exclude)
 
 % DATACLEANUP creates the right data to input to the EDCF function.
 % INPUT
@@ -6,6 +6,9 @@ function [lifetimes, censoring] = datacleanup(d0,dstart,obj,th,exclude)
 %	DSTART - observation start time in the format 'yyyyMMdd HHmmss'
 %	OBJ - SSA object
 %	TH - distance threshold (in pixels) for connecting two surface events
+%	ET - time between the start of exposures, in seconds
+%	FF - the minimum number of frames that an object must appear on to be
+%	included in the lifetime data.
 % 	EXCLUDE - exclude events of unknown lifetime ('yes') or include ('no')
 % OUTPUT
 % 	LIFETIMES - lifetime data to input into ecdf
@@ -16,8 +19,10 @@ tstart = datetime(dstart,'InputFormat','yyyyMMdd HHmmss');
 L = datenum(tstart - t0)*24*60*60;
 
 spots = connectEvents(obj,th);
-[C,cens] = getCoordinates(obj,spots,exclude);
-lengths = cellfun(@length,C);
-lifetimes = C(lengths >= 3);
-censoring = cens(lengths >= 3);
+if nargout > 1
+	[C,censoring] = getCoordinates(obj,spots,exclude,ff);
+else
+	C = getCoordinates(obj,spots,exclude,ff);
+end
+lifetimes = cellfun(@length,C)*et + L;
 end
