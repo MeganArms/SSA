eb61b83a91b225a58bee21c09e2b447ddc35ea7b
MeganArms <megjarm1991@gmail.com>
2017-09-07 10:59:44 -0400

    Drift correction

--- helper/driftcorr.m
+++ helper/driftcorr.m
@@ -1,65 +1,112 @@
-% % Drift Correction
-% 
-% % Use Frame to collect the coordinates of the particles on the frames in
-% % the interval of interest, want to minimize the for loops
-% T = floor(Nframes/10);
-% m = 512;
-% N = zeros(m,m,10);
-% % N = zeros(m,m,9);
-% D = zeros(m,m,size(N,3)-1);
-% driftobjs = [];
-% for k = 1:size(N,3)
-%     startframe = (k-1)*T+1;
-%     endframe = k*T;
-%     interval = startframe:endframe;
-%     X = round(objs_link(1,cell2mat(Frame(interval))),1);
-%     Y = round(objs_link(2,cell2mat(Frame(interval))),1);
-% %     X = round(objs_link(1,Frame{k}),1);
-% %     Y = round(objs_link(2,Frame{k}),1);
-% %     [N(:,:,k),Xedges,Yedges] = histcounts2(X,Y,m);
-%     % Perform cross-correlation between each of the projections
-%     if k ~= 1
-%         tmp = xcorr2(N(:,:,k-1),N(:,:,k));
-%         D(:,:,k-1) = tmp(257:512+256,257:512+256);
-%     else
-%         continue
-%     end
-%     % Get neighborhoods
-%     img = D(:,:,k-1);
-%     driftobjs = [driftobjs,analysis_rpstyle(img)];
-% end
-% 
-% driftobj_link = nnlink_rp(driftobjs, 10, 1, true);
+% Drift Correction
+
+% Use Frame to collect the coordinates of the particles on the frames in
+% the interval of interest, want to minimize the for loops
+ps = 160; % nm, pixel size
+slices = 10;
+T = floor(Nframes/slices);
+m = 512;
+N = zeros(m,m,slices);
+% N = zeros(m,m,9);
+D = zeros(m,m,slices-1);
+driftobjs = [];
+for k = 1:size(N,3)
+    startframe = (k-1)*T+1;
+    endframe = k*T;
+    interval = startframe:endframe;
+    X = round(objs_link(1,cell2mat(Frame(interval))));
+    Y = round(objs_link(2,cell2mat(Frame(interval))));
+%     X = round(objs_link(1,Frame{k}),1);
+%     Y = round(objs_link(2,Frame{k}),1);
+    [N(:,:,k),Xedges,Yedges] = histcounts2(X,Y,m);
+    % Perform cross-correlation between each of the projections
+    if k == 1
+        continue
+    else
+        tmp = xcorr2(N(:,:,k-1),N(:,:,k));
+        D(:,:,k-1) = tmp(257:512+256,257:512+256);
+    end
+    % Get neighborhoods
+    img = D(:,:,k-1);
+    tmpobj = analysis_rpstyle(img);
+    tmpobj(5,:) = k-1;
+    driftobjs = [driftobjs,tmpobj];
+end
+
+driftobjs_link = nnlink_rp(driftobjs, 10, 1, true);
 
 % Now everything is linked, get the drift in each direction at each time
 % interval
-numTrajs = max(objs_link(6,:));
-xcoords = objs_link(1,:);
-ycoords = objs_link(2,:);
-trkID = objs_link(6,:);
-XC = cell(numTrajs,1); YC = XC;
+numTrajs = max(driftobjs_link(6,:));
+xcoords = driftobjs_link(1,:);
+ycoords = driftobjs_link(2,:);
+trkID = driftobjs_link(6,:);
+XC = NaN(numTrajs,slices); YC = XC;
 for k = 1:numTrajs
-    XC{k} = objs_link(1,trkID == k);
-    YC{k} = objs_link(2,trkID == k);
+    currTraj = driftobjs_link(1:2,trkID == k);
+    firstframe = min(driftobjs_link(5,trkID == k));
+    trajInd = firstframe:firstframe+size(currTraj,2)-1;
+    XC(k,trajInd) = currTraj(1,:);
+    YC(k,trajInd) = currTraj(2,:);
 end
 
-dXC = cellfun(@diff,XC,'UniformOutput',0); % This isn't right! is it?
-% yeah it is because I want the drift between points, not the points
-% themselves
-dYC = cellfun(@diff,YC,'UniformOutput',0);
-lengths = cellfun(@length,XC);
-longest = max(lengths);
+dXC = diff(XC,1,2); dYC = diff(YC,1,2);
+xdriftvals = mean(dXC,1,'omitnan')*ps; 
+xsem = xdriftvals./sqrt(sum(~isnan(dXC),1));
+ydriftvals = mean(dYC,1,'omitnan')*ps;
+ysem = ydriftvals./sqrt(sum(~isnan(dYC),1));
 
-dXCpadded = cellfun(@(x) padarray(x,[0 longest-length(x)],NaN,'post'),dXC,'UniformOutput',0);
-dYCpadded = cellfun(@(x) padarray(x,[0 longest-length(x)],NaN,'post'),dYC,'UniformOutput',0);
-dXCmat = cat(1,dXCpadded{:}); YCmat = cat(1,dYCpadded{:});
-
-xdriftvals = mean(dXCmat,1,'omitnan')*160;
-ydriftvals = mean(YCmat,1,'omitnan')*160;
-
-timeInt = 200:200:longest*200;
-figure,plot(timeInt,xdriftvals,'b.-'),hold on, plot(timeInt,ydriftvals,'r.-'),hold off
+frameInt = 2;
+timeInt = frameInt*T:frameInt*T:(slices-1)*frameInt*T;
+figure,errorbar(timeInt,xdriftvals,xsem,xsem,'b.-')
+hold on, errorbar(timeInt,ydriftvals,ysem,ysem,'r.-'), hold off
 legend('x','y','Location','best'), xlabel('Time Interval, t (s)'), ylabel('Drift Value (nm)')
+title([num2str(slices),' slices']);
 
 % Cubic spline interpolation
+timeVec = 2:frameInt:(Nframes-1)*frameInt;
+xq = interp1(timeInt,xdriftvals,timeVec,'spline');
+yq = interp1(timeInt,ydriftvals,timeVec,'spline');
+figure,plot(timeVec,xq,'b.-',timeVec,yq,'r.-')
+
+% Apply correction
+% Remove trajectories less than 2 frames
+Clong = C(cellfun(@(x) size(x,2),C)>1);
+Flong = F(cellfun(@length,F)>1);
+% Find location in time
+XC = NaN(length(Clong),Nframes); YC = XC;
+for k = 1:length(Flong)
+    inds = Flong{k};
+    XC(k,inds) = Clong{k}(1,:);
+    YC(k,inds) = Clong{k}(2,:);
+end
+% Correct the displacements
+dXC = diff(XC,1,2)*ps; dYC = diff(YC,1,2)*ps;
+xcorr = [NaN(1,T-1),cumsum(xq(T:end))]; ycorr = [NaN(1,T-1),cumsum(yq(T:end))];
+XCcorr = XC - xcorr; YCcorr = YC - ycorr;
+% dXCcorr = dXC - xq; dYCcorr = dYC - yq;
+% Convert displacements back into coordinates - this only corrects drift
+% after the initial point. Need to correct it over the whole thing, i.e.
+% even the first point in a trajectory that appears in the middle of the
+% video has experienced drift.
+firstind = find(~isnan(XC(k,:)),1);
+firstcoord = XC(k,firstind);
+XCcorr(k,firstind:end) = [firstcoord, firstcoord + cumsum(dXCcorr(k,firstind:end,'omitnan'))];
+
+XCcorr = XC + xcorr;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
