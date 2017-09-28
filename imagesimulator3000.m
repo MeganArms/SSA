d4eaed9631b6f54730e48485b76583fea3062d07
MeganArms <megjarm1991@gmail.com>
2017-09-28 12:45:16 -0400

    Add image simulation

--- imagesimulator3000.m
+++ imagesimulator3000.m
@@ -1,15 +1,5 @@
-function [IMG,CCD_IMG,F0,Ff] = imagesimulator3000(m,b,imstd,ib,snr,NA,lambda,ps,gs,sf,iter,varargin)
-% % Define the integral function for later substitution
-% syms x y I mux muy a ib
-% % int_fx = ib*x - (2^(1/2)*I*pi^(1/2)*exp(-muy^2/(2*a^2))*exp(-y^2/(2*a^2))...
-% %     *exp((muy*y)/a^2)*erf((2^(1/2)*(mux - x)*(1/a^2)^(1/2))/2))/...
-% %     (2*(1/a^2)^(1/2));
-% int_f = ib*x*y - (I*pi*erf(-(2^(1/2)*((muy*1i)/a^2 - (y*1i)/a^2))...
-%     /(2*(-1/a^2)^(1/2)))*erf((2^(1/2)*(mux - x)*(1/a^2)^(1/2))/2)*1i)...
-%     /(2*(1/a^2)^(1/2)*(-1/a^2)^(1/2));
-
-
-
+function [IMG,CCD_IMG] = imagesimulator3000(m,b,imstd,ib,snr,NA,lambda,...
+    ps,gs,sf,iter,varargin)
 % Simulate particle on high-resolution grid (HRG). A pixel is 160 nm, so
 % the grid will be 1/160 pixels in resolution. 
 % Maybe worth considering the pros and cons of using units of pixels or
@@ -40,45 +30,13 @@ for k = 1:iter
     I = poissrnd(snr*imstd);
 
     % Simulate particle on an HRG
-    ip = I*exp(-((xg-mux).^2 + (yg-muy).^2)/(2*a^2)) + ib; % constant bg doesn't seem right
-    % ip = f(xg,yg,I,ib,a,mux,muy);
+    % constant bg doesn't seem right
+    ip = I*exp(-((xg-mux).^2 + (yg-muy).^2)/(2*a^2)) + ib; 
     
     % Add noise to HRG
     IMG(:,:,k) = ip + (m*ip+b).*normrnd(0,1,size(ip));
     
     % Sum to obtain pixel-resolution grid
-    CCD_IMG = reshape(mean(im2col(IMG,[ps^2/sf^2 ps^2/sf^2],'distinct'),1),[gs,gs]);
-   
-    % Integrate to obtain pixel-resolution grid
-%     F = matlabFunction(subs(int_f));
-%     x0 = 1:ps:gs*ps; xf = ps:ps:ps*gs; y0 = x0; yf = xf;
-%     [x0g,y0g] = meshgrid(x0,y0); [xfg,yfg] = meshgrid(xf,yf);
-%     CCD_IMG = F(xfg,yfg) - F(x0g,y0g);
-    
-    % No anonymous or symbolic function method - hold on. There's no noise
-    % in this function.
-%     x0 = 1:ps:gs*ps; xf = ps:ps:ps*gs; y0 = x0; yf = xf;
-%     [x0g,y0g] = meshgrid(x0,y0); [xfg,yfg] = meshgrid(xf,yf);
-%     F0 = ib.*x0g.*y0g - (I*pi*erf(-(2^(1/2)*((muy*1i)/a^2 - (y0g*1i)...
-%         /a^2))/(2*(-1/a^2)^(1/2))).*erf((2^(1/2)*(mux - x0g)*...
-%         (1/a^2)^(1/2))/2)*1i)/(2*(1/a^2)^(1/2)*(-1/a^2)^(1/2));
-%     Ff = ib.*xfg.*yfg - (I*pi*erf(-(2^(1/2)*((muy*1i)/a^2 - (yfg*1i)...
-%         /a^2))/(2*(-1/a^2)^(1/2))).*erf((2^(1/2)*(mux - xfg)*...
-%         (1/a^2)^(1/2))/2)*1i)/(2*(1/a^2)^(1/2)*(-1/a^2)^(1/2));
-%     CCD_IMG(:,:,k) = Ff - F0;
+    CCD_IMG = reshape(mean(im2col(IMG,[ps^2/sf^2 ps^2/sf^2],'distinct'),...
+        1),[gs,gs]);
 end
-
-
-
-% Fx_subs = subs(Fx_definite,x,[x0,xf]);
-% Fx = Fx_subs(2,:) - Fx_subs(1,:);
-% intf
-% 
-% int_fx = matlabFunction(subs(int_fx));
-% 
-% int_f = matlabFunction(subs(int_f)); % use this to integrate to a low res grid
-% 
-% 
-% B = im2col(IMG,[ps ps],'distinct');
-% % Either need to rewrite the function to take columns or to take block
-% % structs
