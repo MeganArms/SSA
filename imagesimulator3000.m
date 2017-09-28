6d5d84bf391a4c26033444fcf6ddec036a4689bb
MeganArms <megjarm1991@gmail.com>
2017-09-28 14:27:52 -0400

    Add image simulation

--- imagesimulator3000.m
+++ imagesimulator3000.m
@@ -1,4 +1,4 @@
-function [IMG,CCD_IMG] = imagesimulator3000(m,b,imstd,ib,snr,NA,lambda,...
+function [IMG,ip] = imagesimulator3000(m,b,imstd,ib,snr,NA,lambda,...
     ps,gs,sf,iter,varargin)
 % Simulate particle on high-resolution grid (HRG). A pixel is 160 nm, so
 % the grid will be 1/160 pixels in resolution. 
@@ -22,21 +22,24 @@ else
 end
 
 % Run simulation
-IMG = zeros(length(xx),length(xx),iter);
-CCD_IMG = zeros(gs,gs,iter); 
+ip = zeros(length(xx),length(xx),iter);
+IMG = zeros(gs,gs,iter); 
 % f = @(x,y,A,C,a,mux,muy) A*exp(-((x-mux).^2+(y-muy).^2)/(2*a^2)) + C;
 for k = 1:iter
     % Determine the peak signal intensity. SNR is user input.
     I = poissrnd(snr*imstd);
 
     % Simulate particle on an HRG
-    % constant bg doesn't seem right
-    ip = I*exp(-((xg-mux).^2 + (yg-muy).^2)/(2*a^2)) + ib; 
+    % constant bg doesn't seem right. but this is the noiseless image.
+    ip(:,:,k) = I*exp(-((xg-mux).^2 + (yg-muy).^2)/(2*a^2)) + ib; 
     
     % Add noise to HRG
-    IMG(:,:,k) = ip + (m*ip+b).*normrnd(0,1,size(ip));
+    % IMG(:,:,k) = ip + (m*ip+b).*normrnd(0,1,size(ip));
     
     % Sum to obtain pixel-resolution grid
-    CCD_IMG = reshape(mean(im2col(IMG,[ps^2/sf^2 ps^2/sf^2],'distinct'),...
-        1),[gs,gs]);
+%     CCD_IMG(:,:,k) = reshape(mean(im2col(IMG(:,:,k),[ps^2/sf^2 ps^2/sf^2],...
+%         'distinct'),1),[gs,gs]);
+    im = reshape(mean(im2col(ip,[ps^2/sf^2 ps^2/sf^2],...
+        'distinct'),1),[gs,gs]);
+    IMG(:,:,k) = im + (m*im+b).*normrnd(0,1,size(im));
 end
